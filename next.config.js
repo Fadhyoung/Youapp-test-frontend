85e4e38bae94b99f3c075d68fcc716021b0be8eb
fanuhi <fadhlinurhimawan@gmail.com>
2024-11-30 16:12:20 +0700

    configure CI/CD

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/Youapp-test-frontend', 
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
