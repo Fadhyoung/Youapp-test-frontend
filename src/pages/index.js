7dfee060a82d0b116ff131fe39d11f353913087e
fanuhi <fadhlinurhimawan@gmail.com>
2024-11-30 16:47:09 +0700

    fix error message CI/CD pt.4

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
         console.error('Failed to fetch profile data:', error);
       }
 
-  }, [syncProfile])
+  }, [Profile, Edit, DisplayName, Birthday, Horoscope, Zodiac, Height, Weight, Interests, syncProfile])
 
   const handleInterest = () => {
     event.preventDefault();
